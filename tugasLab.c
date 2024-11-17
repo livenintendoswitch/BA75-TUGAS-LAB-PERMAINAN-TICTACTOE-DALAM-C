878faf82fc1d8f407e494be253df74e58f126305
livenintendoswitch <leonard.suwanta3@gmail.com>
2024-11-17 21:41:20 +0700

    fitur menu baru

--- tugasLab.c
+++ tugasLab.c
@@ -2,8 +2,42 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <unistd.h>
+#include <ncurses.h>
+#ifdef _WIN32
+#include <conio.h>
+#endif
+void systemClear(){
+    system("clear");
+    system("cls");
+}
 int game(){
 //masukin gamenya disini
+}
+//matiin cursor
+void disableCursor() {
+     printf("\033[?25l");
+}
+//main menu
+int mainMenu(int selection){
+    systemClear();
+    printf("/$$$$$$$$ /$$$$$$  /$$$$$$        /$$$$$$$$ /$$$$$$   /$$$$$$        /$$$$$$$$ /$$$$$$  /$$$$$$$$\n");
+    printf("|__  $$__/|_  $$_/ /$$__  $$      |__  $$__//$$__  $$ /$$__  $$      |__  $$__//$$__  $$| $$_____/ \n");
+    printf("   | $$     | $$  | $$  \\__/         | $$  | $$  \\ $$| $$  \\__/         | $$  | $$  \\ $$| $$      \n");
+    printf("   | $$     | $$  | $$               | $$  | $$$$$$$$| $$               | $$  | $$  | $$| $$$$$   \n");
+    printf("   | $$     | $$  | $$               | $$  | $$__  $$| $$               | $$  | $$  | $$| $$__/   \n");
+    printf("   | $$     | $$  | $$    $$         | $$  | $$  | $$| $$    $$         | $$  | $$  | $$| $$      \n");
+    printf("   | $$    /$$$$$$|  $$$$$$/         | $$  | $$  | $$|  $$$$$$/         | $$  |  $$$$$$/| $$$$$$$$\n");
+    printf("   |__/   |______/ \\______/          |__/  |__/  |__/ \\______/          |__/   \\______/ |________/ \n");
+    puts("group nasi daun ayam geprek");
+    puts("note: mohon menggunakan program ini dalam full screen");
+    puts("selamat datang di permainan tic tac toe pilih salah satu opsi:");
+    printf("\033[0;33mHINT: GUNAKAN W UNTUK SELEKSI KEATAS DAN S UNTUK SELEKSI KEBAWAH\n\033[0m");
+    if (selection == 0) {
+        printf("\033[0;32mmulai permainan\033[0m \nkeluar dari game");
+    }
+    else {
+         printf("mulai permainan\033[0;32m \nkeluar dari game \033[0m");
+    }
 }
 
 //menu difficulty
@@ -11,7 +45,7 @@ int difficultyMenu() {
     int choice;
     system("cls");//buat windows
     system("clear");//buat linux
-     printf(" __       __   ______   _______   ________        _______   ________  _______   __       __   ______   ______  __    __   ______   __    __ \n");
+    printf(" __       __   ______   _______   ________        _______   ________  _______   __       __   ______   ______  __    __   ______   __    __ \n");
     printf("/  \\     /  | /      \\ /       \\ /        |      /       \\ /        |/       \\ /  \\     /  | /      \\ /      |/  \\  /  | /      \\ /  \\  /  | \n");
     printf("$$  \\   /$$ |/$$$$$$  |$$$$$$$  |$$$$$$$$/       $$$$$$$  |$$$$$$$$/ $$$$$$$  |$$  \\   /$$ |/$$$$$$  |$$$$$$/ $$  \\ $$ |/$$$$$$  |$$  \\ $$ | \n");
     printf("$$$  \\ /$$$ |$$ |  $$ |$$ |  $$ |$$ |__          $$ |__$$ |$$ |__    $$ |__$$ |$$$  \\ /$$$ |$$ |__$$ |  $$ |  $$$  \\$$ |$$ |__$$ |$$$  \\$$ | \n");
@@ -53,45 +87,42 @@ int difficultyMenu() {
 
 int main() {
     //declare variable disini
-    int valueWhileLoop = 1, choice;
-
+    int valueWhileLoop = 1, choice, selection = 0;
+    char key;
+    initscr();
+    nodelay(stdscr, TRUE);
     while(valueWhileLoop){
-    //user interface
-    printf("/$$$$$$$$ /$$$$$$  /$$$$$$        /$$$$$$$$ /$$$$$$   /$$$$$$        /$$$$$$$$ /$$$$$$  /$$$$$$$$\n");
-    printf("|__  $$__/|_  $$_/ /$$__  $$      |__  $$__//$$__  $$ /$$__  $$      |__  $$__//$$__  $$| $$_____/ \n");
-    printf("   | $$     | $$  | $$  \\__/         | $$  | $$  \\ $$| $$  \\__/         | $$  | $$  \\ $$| $$      \n");
-    printf("   | $$     | $$  | $$               | $$  | $$$$$$$$| $$               | $$  | $$  | $$| $$$$$   \n");
-    printf("   | $$     | $$  | $$               | $$  | $$__  $$| $$               | $$  | $$  | $$| $$__/   \n");
-    printf("   | $$     | $$  | $$    $$         | $$  | $$  | $$| $$    $$         | $$  | $$  | $$| $$      \n");
-    printf("   | $$    /$$$$$$|  $$$$$$/         | $$  | $$  | $$|  $$$$$$/         | $$  |  $$$$$$/| $$$$$$$$\n");
-    printf("   |__/   |______/ \\______/          |__/  |__/  |__/ \\______/          |__/   \\______/ |________/ \n");
-    puts("group nasi daun ayam geprek");
-    puts("note: mohon menggunakan program ini dalam full screen");
-    puts("selamat datang di permainan tic tac toe pilih salah satu opsi:");
-    puts("1) main");
-    puts("2) keluar dari program");
-    printf("apa yang anda ingin lakukan sekarang? : ");
-    scanf("%d", &choice);
-    switch(choice){
-        case 1: 
-            difficultyMenu();
-            break;
-        case 2: 
-            system("cls");//buat windows
-            system("clear");//buat linux
-            puts("keluar...");
-            puts("terima kasih telah memainkan game kami!");
-            sleep(2);
-            valueWhileLoop = 0;
-            break;
-        default:    
-            system("cls");//buat windows
-            system("clear");//buat linux
-            puts("jawaban tidak valid!");
-            puts("loading.....");
-            sleep(2);
-            break;
+    mainMenu(selection);
+    system("stty -echo");//buat linux
+    disableCursor();
+    key = getch();
+    if(key == 'w' || key == 'W') {
+        if(selection > 0) {
+            selection--;
+        }
+    }   
+        else if(key == 's' || key == 'S'){
+            if(selection < 1) {
+                selection++;
+            }
+            else if(key == '\n'){
+                if(selection == 0){
+                systemClear();
+                puts("loading.....");
+                sleep(2);
+                difficultyMenu();
+                }
+                else if(selection == 1){
+                systemClear();
+                puts("terima kasih telah memainkan game kami :)");
+                sleep(2);
+                valueWhileLoop = 0;
+                }
+            }
+            
     }
+
+
 
     }
     return 0;
