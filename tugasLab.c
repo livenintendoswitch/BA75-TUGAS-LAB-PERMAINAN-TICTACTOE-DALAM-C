41f9b0561c12d845444bed67d2a48420bacc5fbe
livenintendoswitch <leonard.suwanta3@gmail.com>
2024-12-28 22:53:12 +0700

    seamless menu udah bisa cuma balik ke menu utama masih masalah

--- tugasLab.c
+++ tugasLab.c
@@ -2,74 +2,100 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <unistd.h>
-#if defined(_WIN32) || defined(_WIN64) 
+#include <string.h>
+#if _WIN32 || _WIN64
 #include <conio.h>
-#else 
+#else
 #include <termios.h>
-//getch() alternatif untuk linux
-//sumber = https://gist.github.com/ninovsnino/f910701ea912f03b73a1d0895d213b0e
-static struct termios old, new;
+//dari https://stackoverflow.com/questions/7469139/what-is-the-equivalent-to-getch-getche-in-linux
+static struct termios old, current;
 
 /* Initialize new terminal i/o settings */
-void initTermios(int echo){
+void initTermios(int echo) 
+{
   tcgetattr(0, &old); /* grab old terminal i/o settings */
-  new = old; /* make new settings same as old settings */
-  new.c_lflag &= ~ICANON; /* disable buffered i/o */
-  new.c_lflag &= echo ? ECHO : ~ECHO; /* set echo mode */
-  tcsetattr(0, TCSANOW, &new); /* use these new terminal i/o settings now */
+  current = old; /* make new settings same as old settings */
+  current.c_lflag &= ~ICANON; /* disable buffered i/o */
+  if (echo) {
+      current.c_lflag |= ECHO; /* set echo mode */
+  } else {
+      current.c_lflag &= ~ECHO; /* set no echo mode */
+  }
+  tcsetattr(0, TCSANOW, &current); /* use these new terminal i/o settings now */
 }
 
-void resetTermios(void) {
-tcsetattr(0, TCSANOW, &old);
+/* Restore old terminal i/o settings */
+void resetTermios(void) 
+{
+  tcsetattr(0, TCSANOW, &old);
 }
 
 /* Read 1 character - echo defines echo mode */
-char getch_(int echo){
+char getch_(int echo) 
+{
   char ch;
   initTermios(echo);
   ch = getchar();
   resetTermios();
   return ch;
 }
+
+/* Read 1 character without echo */
 char getch(void) 
 {
   return getch_(0);
 }
+
+/* Read 1 character with echo */
+char getche(void) 
+{
+  return getch_(1);
+}
 #endif
-//global variable
- int menuSelection = 0, diffSelection = 0, leadSelection = 0, modeSelection = 0;
-//USER INTERFACE
-void clearScreen(){
-    #if defined(_WIN32) || defined(_WIN64)
+
+char getKeyboard() {
+     #if _WIN32 |_WIN64
+    return getch();
+    #else
+    return getch();
+    #endif 
+}
+
+void clearScreen() {
+    #if _WIN32 || _WIN64
     system("cls");
     #else
     system("clear");
     #endif
 }
 //kontrol dimasukin ke fungsi biar bisa dipake ulang
-int navControls(int *selection, int Options) {
-    char key = getch();
-     if(key == 'w' || key == 'W') {
-            if(*selection > 0) {
-                (*selection)--;
-            }
-        } else if(key == 's' || key == 'S') {
-            if(*selection < Options - 1) {
-                (*selection)++;
-            }
-        } else if(key == '\n') {
-            return 1;
-        }
-    return 0;
+int NavKey(char key, int selection, int options){
+    if (key == 'W' || key == 'w') {
+        selection--;
+    } else if (key == 'S' || key == 's') {
+        selection++;
+    } else if (key == ' ') {
+        return -1; 
+    }
+
+    if (selection < 0) {
+        selection = 0;
+    } else if (selection >= options) {
+        selection = options - 1;
+    }
+
+    return selection;
 }
 
 //matiin cursor
 void disableCursor() {
      printf("\033[?25l");
 }
+void enableCursor() {
+    printf("\033[?25h");
+}
 //main menu
-void mainMenu(int menuSelection){
-    clearScreen();
+void mainMenu(int mainMenuSelection){
     printf("/$$$$$$$$ /$$$$$$  /$$$$$$        /$$$$$$$$ /$$$$$$   /$$$$$$        /$$$$$$$$ /$$$$$$  /$$$$$$$$\n");
     printf("|__  $$__/|_  $$_/ /$$__  $$      |__  $$__//$$__  $$ /$$__  $$      |__  $$__//$$__  $$| $$_____/ \n");
     printf("   | $$     | $$  | $$  \\__/         | $$  | $$  \\ $$| $$  \\__/         | $$  | $$  \\ $$| $$      \n");
@@ -81,20 +107,13 @@ void mainMenu(int menuSelection){
     puts("group nasi daun ayam geprek");
     puts("note: mohon menggunakan program ini dalam full screen");
     puts("selamat datang di permainan tic tac toe pilih salah satu opsi:");
-    printf("\033[0;33mHINT: GUNAKAN W UNTUK SELEKSI KEATAS DAN S UNTUK SELEKSI KEBAWAH\n\033[0m");
-    if (menuSelection == 0) {
-        printf("\033[0;32mmulai permainan\033[0m \nkeluar dari game \nLeaderboard");
-    }
-    else if(menuSelection == 1) {
-        printf("mulai permainan\033[0;32m \nkeluar dari game \033[0m \nLeaderboard");
-    }
-    else if(menuSelection == 2) {
-        printf("mulai permainan \nkeluar dari game \033[0;32m\nLeaderboard\033[0m");
-    }
+    printf("\033[0;33mHINT: gunakan W dan S untuk seleksi dan spasi untuk memilih\n\033[0m");
+    printf("%smulai permainan%s\n", mainMenuSelection == 0 ? "\033[0;32m" : "", mainMenuSelection == 0 ? " <-\033[0m" : "");
+    printf("%sleaderboard%s\n", mainMenuSelection == 1 ? "\033[0;32m" : "", mainMenuSelection == 1 ? " <-\033[0m" : "");
+    printf("%skeluar dari game%s\n", mainMenuSelection == 2 ? "\033[0;32m" : "", mainMenuSelection == 2 ? " <-\033[0m" : "");
 }
 //menu mode
 void gameModeMenu(int modeSelection) {
-    clearScreen();
         printf(" __       __   ______   _______   ________        _______   ________  _______   __       __   ______   ______  __    __   ______   __    __ \n");
         printf("/  \\     /  | /      \\ /       \\ /        |      /       \\ /        |/       \\ /  \\     /  | /      \\ /      |/  \\  /  | /      \\ /  \\  /  | \n");
         printf("$$  \\   /$$ |/$$$$$$  |$$$$$$$  |$$$$$$$$/       $$$$$$$  |$$$$$$$$/ $$$$$$$  |$$  \\   /$$ |/$$$$$$  |$$$$$$/ $$  \\ $$ |/$$$$$$  |$$  \\ $$ | \n");
@@ -104,37 +123,136 @@ void gameModeMenu(int modeSelection) {
         printf("$$ |$$$/ $$ |$$ \\__$$ |$$ |__$$ |$$ |_____       $$ |      $$ |_____ $$ |  $$ |$$ |$$$/ $$ |$$ |  $$ | _$$ |_ $$ |$$$$ |$$ |  $$ |$$ |$$$$ | \n");
         printf("$$ | $/  $$ |$$    $$/ $$    $$/ $$       |      $$ |      $$       |$$ |  $$ |$$ | $/  $$ |$$ |  $$ |/ $$   |$$ | $$$ |$$ |  $$ |$$ | $$$ | \n");
         printf("$$/      $$/  $$$$$$/  $$$$$$$/  $$$$$$$$/       $$/       $$$$$$$$/ $$/   $$/ $$/      $$/ $$/   $$/ $$$$$$/ $$/   $$/ $$/   $$/ $$/   $$/ \n");
-        if (modeSelection == 0) {
-            printf("\033[0;32mmulai permainan\033[0m \nlocal multiplayer \nbalik ke menu utama");
-        }
-        else if (modeSelection == 1) {
-            printf("mulai permainan \n\033[0;32mlocal multiplayer \033[0m \nbalik ke menu utama");
-        }
-        else if (modeSelection == 2) {
-            printf("mulai permainan \nlocal multiplayer  \n\033[0;32mbalik ke menu utama \033[0m");
-        }
+        printf("%smulai permainan%s\n", modeSelection == 0 ? "\033[0;32m" : "", modeSelection == 0 ? " <-\033[0m" : "");
+        printf("%slocal multiplayer %s\n", modeSelection == 1 ? "\033[0;32m" : "", modeSelection == 1 ? " <-\033[0m" : "");
+        printf("%skembali ke menu utama%s\n", modeSelection == 2 ? "\033[0;32m" : "", modeSelection == 2 ? " <-\033[0m" : "");
 }
 
 void difficultyMenu(int diffselection){
-
+    printf("                         __                                                    __                    __     \n");
+    printf("                        /  |                                                  /  |                  /  |    \n");
+    printf(" _____  ____    ______  $$ |  ______   __   __   __   ______   _______        $$ |____    ______   _$$ |_   \n");
+    printf("/     \\/    \\  /      \\ $$ | /      \\ /  | /  | /  | /      \\ /       \\       $$      \\  /      \\ / $$   |  \n");
+    printf("$$$$$$ $$$$  |/$$$$$$  |$$ | $$$$$$  |$$ | $$ | $$ | $$$$$$  |$$$$$$$  |      $$$$$$$  |/$$$$$$  |$$$$$$/   \n");
+    printf("$$ | $$ | $$ |$$    $$ |$$ | /    $$ |$$ | $$ | $$ | /    $$ |$$ |  $$ |      $$ |  $$ |$$ |  $$ |  $$ | __ \n");
+    printf("$$ | $$ | $$ |$$$$$$$$/ $$ |/$$$$$$$ |$$ \\_$$ \\_$$ |/$$$$$$$ |$$ |  $$ |      $$ |__$$ |$$ \\__$$ |  $$ |/  |\n");
+    printf("$$ | $$ | $$ |$$       |$$ |$$    $$ |$$   $$   $$/ $$    $$ |$$ |  $$ |      $$    $$/ $$    $$/   $$  $$/ \n");
+    printf("$$/  $$/  $$/  $$$$$$$/ $$/  $$$$$$$/  $$$$$/$$$$/   $$$$$$$/ $$/   $$/       $$$$$$$/   $$$$$$/     $$$$/  \n");
+    puts("pilih level kesusahan lawan bot");
+    printf("%smudah%s\n", diffselection == 0 ? "\033[0;32m" : "", diffselection == 0 ? " <-\033[0m" : "");
+    printf("%ssusah%s\n", diffselection == 1 ? "\033[0;32m" : "", diffselection == 1 ? " <-\033[0m" : "");
+    printf("%skembali ke menu mode permainan%s\n", diffselection == 2 ? "\033[0;32m" : "", diffselection == 2 ? " <-\033[0m" : "");
 }
-int leaderboardMenu(int *leadSelection) {
-    printf("leaderboard in progress...");
-    sleep(2);
-    clearScreen();
-    return 0;
+
+typedef struct {
+    char name[100];
+    int points;
+} Player;
+
+void selectionSortDescending(Player players[], int numPlayers) {
+    int i,j;
+    for (i = 0; i < numPlayers - 1; i++) {
+        int maxIndex = i;
+        for (j = i + 1; j < numPlayers; j++) {
+            if (players[j].points > players[maxIndex].points) {
+                maxIndex = j;
+            }
+        }
+        
+        if (maxIndex != i) {
+            Player temp = players[i];
+            players[i] = players[maxIndex];
+            players[maxIndex] = temp;
+        }
+    }
+}
+
+void searchPlayer(Player players[], int numPlayers, const char *searchName) {
+    int found = 0;
+    int i;
+    for (i = 0; i < numPlayers; i++) {
+        if (strcmp(players[i].name, searchName) == 0) {
+            printf("pemain '%s' ditemukan dengan %d points.\n", players[i].name, players[i].points);
+            found = 1;
+            break;
+        }
+    }
+
+    if (!found) {
+        printf("pemain '%s' tidak ditemukan di leaderboard \n", searchName);
+    }
+}
+
+void menuLeaderboard() {
+    FILE *file = fopen("leaderboard.txt", "r");
+    if (file == NULL) {
+        printf("Error: file tidak ditemukan\n");
+        return;
+    }
+
+    int numPlayers;
+    fscanf(file, "%d", &numPlayers);
+
+    Player players[numPlayers];
+    int i;
+    for (i = 0; i < numPlayers; i++) {
+        fscanf(file, "%s %d", players[i].name, &players[i].points);
+    }
+
+    fclose(file);
+
+    selectionSortDescending(players, numPlayers);
+
+    int loopingMenu = 1;
+    int Leadselection = 0; 
+    while (loopingMenu) {
+        clearScreen();
+
+        printf("Leaderboard:\n");
+        printf("Rank  pemain Name        Points\n");
+        int i;
+        for (i = 0; i < numPlayers; i++) {
+            printf("%-5d %-18s %d\n", i + 1, players[i].name, players[i].points);
+        }
+
+        printf("\nOpsi:\n");
+         printf("%smencari pemain%s\n", Leadselection == 0 ? "\033[0;32m" : "", Leadselection == 0 ? " <-\033[0m" : "");
+        printf("%skembali ke menu%s\n", Leadselection == 1 ? "\033[0;32m" : "", Leadselection == 1 ? " <-\033[0m" : "");
+
+        char key = getKeyboard(); 
+        int result = NavKey(key, Leadselection, 2); 
+        if (result == -1) { 
+            if (Leadselection == 0) {
+                clearScreen();
+                enableCursor();
+                char searchName[100];
+                printf("\nMasukkan nama pemain yang dicari: ");
+                scanf("%s", searchName);
+                searchPlayer(players, numPlayers, searchName);
+                sleep(5);
+            } else if (Leadselection == 1) {
+                loopingMenu = 0;
+            }
+            } else {
+                 Leadselection = result; 
+                }
+    }
+
+    
+    
 }
 
 void loadingScreen() {
     puts("LOADING");
-    for (int i = 0; i <= 10; i++) {
+    int i,j;
+    for (i = 0; i <= 10; i++) {
         printf("\r[");
         
-        for (int j = 0; j < i; j++) {
+        for (j = 0; j < i; j++) {
             printf("#");
         }
         
-        for (int j = i; j < 10; j++) {
+        for (j = i; j < 10; j++) {
             printf(" ");
         }
         
@@ -147,68 +265,130 @@ void loadingScreen() {
     }
 }
 
+int game(int botDifficulty);
+
 int main() {
-    //declare variable disini
-    int valueWhileLoop = 1;
-    system("stty -echo");//buat linux
-    while(valueWhileLoop){
-        mainMenu(menuSelection);
-        disableCursor();
-        if (navControls(&menuSelection, 3)){
-            switch(menuSelection){
-            case 0:
+    disableCursor();
+    int valueWhileLoop = 1; 
+    int mainMenuSelection = 0; 
+
+    while (valueWhileLoop) {
+        clearScreen();
+        mainMenu(mainMenuSelection);
+        char key = getKeyboard(); 
+        int result = NavKey(key, mainMenuSelection, 3); 
+        if (result == -1) {
+            valueWhileLoop = 0; 
+            break;
+        }
+        else{
+        mainMenuSelection = result;
+        }
+    }
+    switch (mainMenuSelection) {
+        case 0: {
+            int modeSelection = 0; 
+            int loopingModeMenu = 1;  
+            clearScreen();
+            loadingScreen();
+            clearScreen();
+            while (loopingModeMenu) {
                 clearScreen();
-                loadingScreen();
-                clearScreen();
-            while(1) {
                 gameModeMenu(modeSelection);
-                if(navControls(&modeSelection, 3)){
-                    switch(modeSelection){
-                    case 0:
+                char key = getKeyboard(); 
+                int result = NavKey(key, modeSelection, 3); 
+                if (result == -1) {
+                    loopingModeMenu = 0; 
+                }else{
+                modeSelection = result;
+                }
+            }
+                switch (modeSelection) {
+                    case 0: {
                         clearScreen();
                         loadingScreen();
-                        clearScreen();
-                        difficultyMenu(diffSelection);
-                        break;
-                    case 1:
-                        clearScreen();
-                        loadingScreen();
-                        clearScreen();
-                        //game
-                        break;
-                    case 2:
-                        clearScreen();
-                        loadingScreen();
-                        clearScreen();
-                        return 1;
-                        break;
-                        default:
-                        Puts("system failure");
-                        break;                    
+                        int diffSelection = 0; 
+                        int loopingSelectBotDiff = 1;  
+                        while (loopingSelectBotDiff) {
+                            clearScreen();
+                            difficultyMenu(diffSelection);
+                            char key = getKeyboard(); 
+                            int result = NavKey(key, diffSelection, 3); 
+                            if (result == -1) {
+                                loopingSelectBotDiff = 0; 
+                            } else{
+                            diffSelection = result;
+                            }
                         }
-                     }
-                    break;
+                            switch(diffSelection){
+                                case 0:
+                                    clearScreen();
+                                    loadingScreen();
+                                    clearScreen();
+                                    game(-1);
+                                    break;
+                                case 1:
+                                    clearScreen();
+                                    loadingScreen();
+                                    clearScreen();
+                                    enableCursor();
+                                    game(1);
+                                    break;
+                                case 2:
+                                    clearScreen();
+                                    loadingScreen();
+                                    clearScreen();
+                                    loopingModeMenu = 1;
+                                    break;
+                                default:
+                                puts("system failure");       
+                                break;                         
+                            }
+                        break;
+                    }
+                    case 1: {
+                        clearScreen();
+                        loadingScreen();
+                        clearScreen();
+                        game(0);
+                        break;
+                    }
+                    case 2: {
+                        clearScreen();
+                        loadingScreen();
+                        clearScreen();
+                        valueWhileLoop = 1;
+                        printf("valueWhileLoop is set to = %d", valueWhileLoop);
+                        sleep(3);
+                        break;
+                    }
+                    default:
+                        printf("System failure.\n");
+                        break;
                 }
                 break;
-                case 1:
-                    clearScreen();
-                    puts("terima kasih telah memainkan game kami :)");
-                    sleep(2);
-                    valueWhileLoop = 0;
-                    break;
-                case 2: 
-                    clearScreen();
-                    loadingScreen();
-                    clearScreen();
-                    leaderboardMenu(&leadSelection);
-                    break;
-                default:
-                    break;
-            }  
+            }
+        case 1: {
+            int loopingCaseLead = 1;  
+            while (loopingCaseLead) {
+                clearScreen();
+                menuLeaderboard();
+                loopingCaseLead = 0;
+            }
         }
-    return 0;
+        case 2: {
+            clearScreen();
+            printf("Terima kasih telah memainkan game kami :)\n");
+            break;
+        }
+        default:
+            printf("Invalid selection.\n");
+            break;
     }
+
+    return 0;
 }
+
 
 //KOMPONEN GAME
 char arr[10] = {'o', '1', '2', '3', '4', '5', '6', '7', '8', '9'};
@@ -320,7 +500,6 @@ int game(int botDifficulty) {
         showboard();
         if (botDifficulty != 0 && player == 2) {
             printf("Bot is thinking...\n");
-            sleep(1);
             botTurn(botDifficulty);
         } else {
             playerTurn(player);
